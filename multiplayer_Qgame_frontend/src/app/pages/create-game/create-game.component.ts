88ab33f53602a4ffe9eebe82642b6cce16e7cf80
5Rabie5 <n.rabietarboush@gmail.com>
2025-04-03 03:20:03 +0200

    add code Create Game

--- multiplayer_Qgame_frontend/src/app/pages/create-game/create-game.component.ts
+++ multiplayer_Qgame_frontend/src/app/pages/create-game/create-game.component.ts
@@ -1,12 +1,71 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {TranslatePipe, TranslateService} from '@ngx-translate/core';
+
+import {
+  MatFormFieldModule
+} from '@angular/material/form-field';
+import {
+  MatInputModule
+} from '@angular/material/input';
+import {
+  MatSelectModule
+} from '@angular/material/select';
+import {
+  MatCheckboxModule
+} from '@angular/material/checkbox';
+import {
+  MatRadioModule
+} from '@angular/material/radio';
+import {
+  MatButtonModule
+} from '@angular/material/button';
 
 @Component({
-  standalone: true,
   selector: 'app-create-game',
-  imports: [],
+  standalone: true,
   templateUrl: './create-game.component.html',
-  styleUrl: './create-game.component.scss'
+  styleUrls: ['./create-game.component.scss'],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatButtonModule,
+    TranslatePipe
+  ]
 })
 export class CreateGameComponent {
+  pointsOptions = [100, 200, 500, 1000, 'Unlimited'];
+  playerCounts = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Unlimited'];
+  difficulties = ['Easy', 'Medium', 'Hard'];
+  selectedCategories: string[] = [];
+  categories: string[] = [];
+
+  constructor(public translate: TranslateService) {
+    this.loadCategories(translate.currentLang);
+
+    // When language changes → reload categories
+    this.translate.onLangChange.subscribe(lang => {
+      this.loadCategories(lang.lang);
+    });
+  }
+
+  loadCategories(lang: string) {
+    const categoryMap: { [key: string]: string[] } = {
+      en: ['General', 'Science', 'Sports', 'History', 'Music'],
+      ar: ['عام', 'علوم', 'رياضة', 'تاريخ', 'فنون', 'أدب'],
+      de: ['Allgemein', 'Wissenschaft', 'Sport', 'Geschichte']
+    };
+
+    this.categories = categoryMap[lang] || categoryMap['en'];
+  }
 
 }
